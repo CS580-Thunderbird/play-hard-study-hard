5af66338b4e1b7079132f148d14876652e1aa39e
dlin345 <dlin345@gmail.com>
2017-01-20 23:12:09 -0800

    Assignment 3 - Created simple HTTP API in Webcontroller.java

--- src/main/java/edu/cpp/cs580/thunderbird/controller/WebController.java
+++ src/main/java/edu/cpp/cs580/thunderbird/controller/WebController.java
@@ -17,70 +17,78 @@ import edu.cpp.cs580.thunderbird.tools.GoogleOAuth;
 
 @RestController
 public class WebController {
-	
-	String userInfo = "";
-	GoogleOAuth OAuth;
-	
-	/**
-	 * This is Assignent #3
-	 * Done by Nanwarin
-	 */
-	@RequestMapping(value = "/Nanwarin")
-	String helloNan(){
-		return "Hello it's me";
-	}
-	
-	/**
-	 * Assignment 3 part 3
-	 * @return
-	 */
-	@RequestMapping(value = "/Wiehsing", method = RequestMethod.GET)
-	String wiehsingtest(){
-		
-		return "Wie Hsing Li testing";
-	}
-	
-	/**
-	 * After user login with Google Account, will add index.html later	
-	 * @return
-	 * @throws IOException 
-	 */
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	String getHome(@RequestParam("code") String code) throws IOException{
-		userInfo = OAuth.getUserInfoJson(code);
-		System.out.println("Test ----- " + userInfo);
-		return "Welcome !!! !!";
-	}
-	
-	/**
-	 * Require User to authorize account with Google before they can use calendar
-	 * Will come up with better method later
-	 * @throws URISyntaxException
-	 */
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public ResponseEntity<Object> loginGoogle() throws URISyntaxException
-	{
-		OAuth = new GoogleOAuth();
-		String url = OAuth.buildLoginUrl();
-		URI loginURI = new URI(url);
-		
-		HttpHeaders httpHeaders = new HttpHeaders();
-		httpHeaders.setLocation(loginURI);
-		
-		return new ResponseEntity<Object>(httpHeaders, HttpStatus.SEE_OTHER);
-	}
-	
-	//Temporary Usage, Just want to try it out
-	@RequestMapping("/getCppClasses")
-	String getCppClasses(){
-		try {
-			GetCppClasses cppClsss = new GetCppClasses();
-		} catch (IOException e) {
-			System.out.println("Error -- getCppClsses");
-		}
-		
-		return "complete";
-	}
-	
+
+    String userInfo = "";
+    GoogleOAuth OAuth;
+
+    /**
+     * This is Assignent #3
+     * Done by Nanwarin
+     */
+    @RequestMapping(value = "/Nanwarin")
+    String helloNan(){
+        return "Hello it's me";
+    }
+
+    /**
+     * Assignment 3 part 3
+     * @return
+     */
+    @RequestMapping(value = "/Wiehsing", method = RequestMethod.GET)
+    String wiehsingtest(){
+
+        return "Wie Hsing Li testing";
+    }
+
+    /**
+     * Assignment 3 - Diana
+     */
+    @RequestMapping(value = "/Diana", method = RequestMethod.GET)
+    String helloDiana() {
+        return "Hello it's me, Diana";
+    }
+
+    /**
+     * After user login with Google Account, will add index.html later
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    String getHome(@RequestParam("code") String code) throws IOException{
+        userInfo = OAuth.getUserInfoJson(code);
+        System.out.println("Test ----- " + userInfo);
+        return "Welcome !!! !!";
+    }
+
+    /**
+     * Require User to authorize account with Google before they can use calendar
+     * Will come up with better method later
+     * @throws URISyntaxException
+     */
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<Object> loginGoogle() throws URISyntaxException
+    {
+        OAuth = new GoogleOAuth();
+        String url = OAuth.buildLoginUrl();
+        URI loginURI = new URI(url);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(loginURI);
+
+        return new ResponseEntity<Object>(httpHeaders, HttpStatus.SEE_OTHER);
+    }
+
+    //Temporary Usage, Just want to try it out
+    @RequestMapping("/getCppClasses")
+    String getCppClasses(){
+        try {
+            GetCppClasses cppClsss = new GetCppClasses();
+        } catch (IOException e) {
+            System.out.println("Error -- getCppClsses");
+        }
+
+        return "complete";
+    }
+
 }
