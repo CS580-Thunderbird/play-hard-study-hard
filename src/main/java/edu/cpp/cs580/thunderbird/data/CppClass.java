63d5b859a0d2c6b65d12e34c885821f5c9b6c9a3
Nanwarin Chantarutai <nan2iz.ch@gmail.com>
2017-01-23 23:37:21 -0800

    Add CppClassData Model

--- src/main/java/edu/cpp/cs580/thunderbird/data/CppClass.java
+++ src/main/java/edu/cpp/cs580/thunderbird/data/CppClass.java
@@ -8,5 +8,38 @@ package edu.cpp.cs580.thunderbird.data;
 public class CppClass {
 	private String code;
 	private String description;
+	private Time classTime;
+	private String instructor;
 	
+	public void setCode(String code){
+		this.code = code;
+	}
+	
+	public String getCode(){
+		return code;
+	}
+	
+	public void setDescription(String description){
+		this.description = description;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public void setClassTime(Time classTime){
+		this.classTime = classTime;
+	}
+	
+	public Time getClassTime(){
+		return classTime;
+	}
+	
+	public void setInstructor(String instructor){
+		this.instructor = instructor;
+	}
+	
+	public String getInstructor(){
+		return instructor;
+	}
 }
