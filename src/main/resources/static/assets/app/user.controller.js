708821496487fd85f3dcef93a80167bf3e49de71
wiehsingli <wiehsing@gmail.com>
2017-02-02 11:33:49 -0800

    Linked log out button to /signout, minor changes to toolbar buttons

--- src/main/resources/static/assets/app/user.controller.js
+++ src/main/resources/static/assets/app/user.controller.js
@@ -1,12 +1,17 @@
 
 app.controller('userInfo', function ($scope, $http) {
 
-  $scope.loadUsers = function() {
-	   $http.get("data/user")
-	   	.then(function(response) {
-	   		$scope.getUser = response.data;
-	   });
-  }
+	$scope.loadUsers = function() {
+		$http.get("data/user")
+		.then(function(response) {
+			$scope.getUser = response.data;
+	});
+	}
+
+	$scope.logout = function () {
+		window.location = '/signout';
+	};
+
 
   $scope.loadUsers();
 });
