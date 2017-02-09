e6a5fb02a187658ae8b0b63ad117b3a8192abfcc
nan2iz <nan2iz.ch@gmail.com>
2017-02-09 11:22:37 -0800

    fix redirect ip from google oauth

--- src/main/java/edu/cpp/cs580/thunderbird/tools/GoogleOAuth.java
+++ src/main/java/edu/cpp/cs580/thunderbird/tools/GoogleOAuth.java
@@ -35,7 +35,7 @@ public class GoogleOAuth {
 
 	//Redirect URI, after login sucessfully
 	//Local
-	private static final String REDIRECT_URI = "http://localhost:8080/home";
+	private static final String REDIRECT_URI = "http://[::1]:8080/home";
 	
 	//AWS
 	//private static final String REDIRECT_URI = "ec2-52-36-235-139.us-west-2.compute.amazonaws.com:8080/home";
