eae2f7bd1ba540c4bfe7d50236d03794fdfb5e03
kushal-patel <pkushal2010@gmail.com>
2017-02-12 19:25:46 -0800

    Add unit test class and unit tests. Finishes Assignment 6

---
+++ src/test/java/edu/cpp/cs580/thunderbird/CppClassScheduleTest.java
@@ -0,0 +1,39 @@
+package edu.cpp.cs580.thunderbird;
+
+import edu.cpp.cs580.thunderbird.data.CppClassSchedule;
+import edu.cpp.cs580.thunderbird.data.TimeObj;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
+import org.junit.Before;
+
+public class CppClassScheduleTest {
+
+	private CppClassSchedule schedule;
+	
+	@Before
+	public void setup() {
+		
+		String classCode = "CS580";
+		String classDesc = "Advanced Software Engineering";
+		TimeObj classTime = new TimeObj();
+		String instructor = "Yu Sun";
+		String location = "Building 9";
+		
+		schedule = new CppClassSchedule(classCode, classDesc, classTime, instructor, location);
+	}
+	
+	@Test
+	public void testGetCode() {
+		String classCode = schedule.getCode();
+		assertEquals(classCode, "CS580");
+	}
+	
+	@Test
+	public void testSetCode() {
+		schedule.setCode("CS664");
+		String classCode = schedule.getCode();
+		assertEquals(classCode, "CS664");
+	}
+}
