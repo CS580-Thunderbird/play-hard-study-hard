2ab441a6d8417263919c1bb1901618014164cf8c
nan2iz <nan2iz.ch@gmail.com>
2017-02-13 15:23:56 -0800

    Nanwarin Assignment # 6, done

--- src/test/java/edu/cpp/cs580/thunderbird/controller/WebControllerTest.java
+++ src/test/java/edu/cpp/cs580/thunderbird/controller/WebControllerTest.java
@@ -1,6 +1,18 @@
 package edu.cpp.cs580.thunderbird.controller;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import edu.cpp.cs580.thunderbird.tools.GoogleOAuth;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /***
  * Assignment#6 by Nanwarin Chantarutai
@@ -9,9 +21,26 @@ import org.junit.Test;
  */
 public class WebControllerTest {
 	
+	GoogleOAuth mockedOAuth;
+	
+	@Before
+	public void setUp(){
+		mockedOAuth = mock(GoogleOAuth.class);
+	}
+	
 	@Test
-	public static void loginGoogleTest(){
-		
+	public void loginGoogleTest() throws URISyntaxException{
+		System.out.println("Perform login with Google Account Test");
+		mockedOAuth = new GoogleOAuth();
+        String mockedUrl = mockedOAuth.buildLoginUrl();
+        URI mockddloginURI = new URI(mockedUrl);
+
+        HttpHeaders mockedhttpHeaders = new HttpHeaders();
+        mockedhttpHeaders.setLocation(mockddloginURI);
+        
+        assertNotNull(mockedhttpHeaders.getLocation());
+       
+       
 	}
 	
 }
