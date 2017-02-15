01793ff2bc289012c491fc999320b9b9ca7d0323
Diana L <dlin345@gmail.com>
2017-02-15 07:57:30 -0800

    Cleaned up unused code from index.html

--- src/main/resources/static/assets/app/main.controller.js
+++ src/main/resources/static/assets/app/main.controller.js
@@ -56,22 +56,6 @@ app.controller("mainController", function($scope, $mdDialog){
 	};
 	$scope.myDate = new Date();
 
-
-	$scope.showFilterTabDialog = function(ev) {
-		$mdDialog.show({
-			controller: DialogController,
-			templateUrl: 'filterTabDialog.k.html',
-			parent: angular.element(document.body),
-			targetEvent: ev,
-			clickOutsideToClose:true
-		})
-		.then(function(answer) {
-			$scope.status = 'You said the information was "' + answer + '".';
-		}, function() {
-			$scope.status = 'You cancelled the dialog.';
-		});
-	};
-
 	function DialogController($scope, $mdDialog) {
 		$scope.hide = function() {
 			$mdDialog.hide();
