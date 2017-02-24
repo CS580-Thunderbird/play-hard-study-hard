421477a5f850a34004514b9b388838470bde3f36
Nanwarin Chantarutai <nan2iz.ch@gmail.com>
2017-02-23 20:09:34 -0800

    change theme to be blue, as discuss first time

---
+++ src/main/java/edu/cpp/cs580/thunderbird/tools/AdminManager.java
@@ -0,0 +1,12 @@
+package edu.cpp.cs580.thunderbird.tools;
+
+public class AdminManager {
+
+	/**
+	 * Add initial Data for Server
+	 * Initial Class to Database
+	 * Initial Events to Database
+	 * Initial Events organization list database
+	 */
+	
+}
