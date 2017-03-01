a42ac58329012ebdc4ab5fc48366c2719b52e45b
dlin345 <dlin345@gmail.com>
2017-03-01 11:16:37 -0800

    Load events from JSON file to calendar #66   - click 'Update Calendar' button to load from JSON   - loading may take a few seconds

--- src/main/resources/static/assets/app/calendar.js
+++ src/main/resources/static/assets/app/calendar.js
@@ -62,6 +62,8 @@ app.controller('CalendarCtrl', function($scope, $window, moment, calendarConfig,
       }
     ];
 
+    $scope.eventList = vm.events;
+
     vm.calendarView = 'month';
     vm.viewDate = moment().startOf('month').toDate();
 
@@ -94,7 +96,7 @@ app.controller('CalendarCtrl', function($scope, $window, moment, calendarConfig,
       angular.extend(calendarEventTitle, originalEventTitle);
     });
 
-    $scope.addClassToCalendar = function() {
+    $scope.addClassToCalendarCookie = function() {
       vm.events = [];
 
       console.log(document.cookie);
@@ -103,21 +105,9 @@ app.controller('CalendarCtrl', function($scope, $window, moment, calendarConfig,
       for(var i = 0; i < ca.length; i++) {
           var c = ca[i];
           c = c.split("=")[1];
-          addedClass = {
-              title: c,
-              color: { // color for Class event
-                primary: classColor,
-                secondary: 'secondaryColor'
-              },
-              actions: [{
-                label: '<i class=\'glyphicon glyphicon-remove\'></i>',
-                onClick: function(args) {
-                  $scope.deleteEvent();
-                }
-              }],
-              startsAt: moment().startOf('week').add(8, 'hours').toDate(),
-              endsAt: moment().startOf('week').add(9, 'hours').toDate(),
-            };
+
+          var addedClass = $scope.makeEvent(c, moment().startOf('week').add(8, 'hours').toDate(),
+              moment().startOf('week').add(9, 'hours').toDate(), classColor);
 
           if (!ca[i].split("=")[0].includes("expires")) {
             vm.events.push(addedClass);
@@ -125,6 +115,85 @@ app.controller('CalendarCtrl', function($scope, $window, moment, calendarConfig,
       }
 
       document.cookie = "12345=;expires=Thu, 01 Jan 1970 00:00:00 UTC";
+    }
+
+    $scope.loadJSON = function (file, callback) {
+      var xobj = new XMLHttpRequest();
+      xobj.overrideMimeType("application/json");
+      xobj.open('GET', file, true);
+      xobj.onreadystatechange = function () {
+            if (xobj.readyState == 4 && xobj.status == "200") {
+              // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+              callback(xobj.responseText);
+            }
+      };
+      xobj.send(null);
+    }
+
+    $scope.makeEvent = function(title, startTime, endTime, color) {
+      return {
+        title: title,
+        color: { // color for event
+          primary: color,
+          secondary: 'secondaryColor'
+        },
+        actions: [{
+          label: '<i class=\'glyphicon glyphicon-remove\'></i>',
+          onClick: function(args) {
+            $scope.deleteEvent();
+          }
+        }],
+        startsAt: startTime,
+        endsAt: endTime,
+      };
+    }
+
+    $scope.addEventToCalendar = function() {
+
+      // temporary load data from JSON file
+      $scope.asiJSON = "data/sampleASIEvents.json";
+      $scope.loadJSON($scope.asiJSON, function(response) {
+        var actual_JSON = JSON.parse(response);
+
+        actual_JSON.forEach(function(item) {
+          var momentObjStart = moment(item.startsAt, moment.ISO_8601);
+          var momentObjEnd = moment(item.endsAt, moment.ISO_8601);
+
+          var addedEvent = $scope.makeEvent(item.title, momentObjStart, momentObjEnd, asiColor);
+
+          console.log(addedEvent);
+          $scope.eventList.push(addedEvent);
+
+        });
+
+      });
+    }
+
+    $scope.addClassToCalendar = function() {
+
+      // temporary load data from JSON file
+      $scope.cppClassJSON = "data/sampleCppClasses.json";
+      $scope.loadJSON($scope.cppClassJSON, function(response) {
+
+        var actual_JSON = JSON.parse(response);
+
+        actual_JSON.forEach(function(item) {
+          var momentObjStart = moment(item.start_date, 'YYYY-MM-DD');
+          var momentObjEnd = moment(item.end_date, 'YYYY-MM-DD');
+
+          var addedClass = $scope.makeEvent(item.code, momentObjStart, momentObjEnd, classColor);
+
+          console.log(addedClass);
+          $scope.eventList.push(addedClass);
+
+        });
+
+      });
+    }
+
+    $scope.addToCalendar = function () {
+      $scope.addClassToCalendar();
+      $scope.addEventToCalendar();
     }
 
   });
